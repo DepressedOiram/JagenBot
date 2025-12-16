4c173ff0370160f8721a57df70393d3e16c58fc7
DepressedOiram <zmarioblue64@gmail.com>
2025-12-16 17:23:59 -0600

    FEH Calendar Update
    - Updated the monthly calendar for FEH

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ fe7.special["denning"] = denning
 async def calendar(ctx):
     embed = maji.Embed()
     
-    embed.attach("image", "https://pbs.twimg.com/media/G5Xm4NzaIAA82NJ?format=jpg&name=medium")
+    embed.attach("image", "https://pbs.twimg.com/media/G7yFovvaMAA0hRw.jpg")
     
     await embed.send(ctx)
 
